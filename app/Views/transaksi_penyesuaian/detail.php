4aa3e2491997348608fbcdf52ba8488523effde4
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-29 14:14:29 +0700

    Menambahkan title pada Detail Transaksi Penyesuaian

--- app/Views/transaksi_penyesuaian/detail.php
+++ app/Views/transaksi_penyesuaian/detail.php
@@ -1,5 +1,9 @@
 <?= $this->extend('layouts/main') ?>
 
+<?= $this->section('title'); ?>
+Detail Transaksi Penyesuaian
+<?= $this->endSection(); ?>
+
 <?= $this->section('content') ?>
 <div class="container-fluid">
     <div class="d-flex align-items-center mb-4">
