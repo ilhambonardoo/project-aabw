ff90f6c7a6d398180bcff83ace7e3a87b9715d61
ilhambonardoo <ilhambonardo123@gmail.com>
2026-03-29 15:20:49 +0700

    buat controller untuk EDIT masing-masing akun dengan validasi rules sesuai tabel transaksi

--- app/Controllers/Akun2Controller.php
+++ app/Controllers/Akun2Controller.php
@@ -36,6 +36,28 @@ class Akun2Controller extends BaseController
     }
 
     public function store(){
+        $rules = [
+            'id_akun_1' => [
+                'label' => 'Induk Klasifikasi',
+                'rules' => 'required|integer'
+            ],
+            'kode_akun_2' => [
+                'label' => 'Kode Akun 2',
+                'rules' => 'required|numeric|is_unique[akun_2.kode_akun_2]',
+                'errors' => [
+                    'is_unique' => '{field} sudah terdaftar dalam sistem'
+                ]
+            ],
+            'nama_akun_2' => [
+                'label' => 'Nama Akun 2',
+                'rules' => 'required|min_length[3]|max_length[100]'
+            ]
+        ];
+
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
         $this->akun2model->save( [
             'id_akun_1' => $this->request->getPost('id_akun_1'),
             'kode_akun_2' => $this->request->getPost('kode_akun_2'),
@@ -43,6 +65,60 @@ class Akun2Controller extends BaseController
         ]);
 
         return redirect()->to('/akun2')->with('success', 'Data Akun 2 berhasil ditambahkan!');
+    }
+
+    public function edit($id){
+        $akun2 = $this->akun2model->find($id);
+
+        if (!$akun2) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $data = [
+            'title' => 'Edit Data Akun 2',
+            'akun2' => $akun2,
+            'akun1' => $this->akun1model->findAll()
+        ];
+
+        return view('akun2/edit', $data);
+    }
+
+    public function update($id){
+        $akun2 = $this->akun2model->find($id);
+
+        if (!$akun2) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $rules = [
+            'id_akun_1' => [
+                'label' => 'Induk Klasifikasi',
+                'rules' => 'required|integer'
+            ],
+            'kode_akun_2' => [
+                'label' => 'Kode Akun 2',
+                'rules' => 'required|numeric|is_unique[akun_2.kode_akun_2,id,' . $id . ']',
+                'errors' => [
+                    'is_unique' => '{field} sudah terdaftar dalam sistem'
+                ]
+            ],
+            'nama_akun_2' => [
+                'label' => 'Nama Akun 2',
+                'rules' => 'required|min_length[3]|max_length[100]'
+            ]
+        ];
+
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
+        $this->akun2model->update($id, [
+            'id_akun_1' => $this->request->getPost('id_akun_1'),
+            'kode_akun_2' => $this->request->getPost('kode_akun_2'),
+            'nama_akun_2' => $this->request->getPost('nama_akun_2')
+        ]);
+
+        return redirect()->to('/akun2')->with('success', 'Data Akun 2 berhasil diperbarui!');
     }
 
     public function delete($id){
--- app/Controllers/Akun3Controller.php
+++ app/Controllers/Akun3Controller.php
@@ -36,6 +36,32 @@ class Akun3Controller extends BaseController
     }
 
     public function store(){
+        $rules = [
+            'id_akun_2' => [
+                'label' => 'Induk Golongan',
+                'rules' => 'required|integer'
+            ],
+            'kode_akun_3' => [
+                'label' => 'Kode Akun 3',
+                'rules' => 'required|numeric|is_unique[akun_3.kode_akun_3]',
+                'errors' => [
+                    'is_unique' => '{field} sudah terdaftar dalam sistem'
+                ]
+            ],
+            'nama_akun_3' => [
+                'label' => 'Nama Akun 3',
+                'rules' => 'required|min_length[3]|max_length[100]'
+            ],
+            'saldo_normal' => [
+                'label' => 'Saldo Normal',
+                'rules' => 'required|in_list[Debit,Kredit]'
+            ]
+        ];
+
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
         $this->akun3model->save([
             'id_akun_2'    => $this->request->getPost('id_akun_2'),
             'kode_akun_3'  => $this->request->getPost('kode_akun_3'),
@@ -46,8 +72,67 @@ class Akun3Controller extends BaseController
         return redirect()->to('/akun3')->with('success', 'Data Akun 3 berhasil ditambahkan!');
     }
 
+    public function edit($id){
+        $akun3 = $this->akun3model->find($id);
+
+        if (!$akun3) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $data = [
+            'title' => 'Edit Data Akun 3',
+            'akun3' => $akun3,
+            'akun2' => $this->akun2model->getAkun2WithAkun1()
+        ];
+
+        return view('akun3/edit', $data);
+    }
+
+    public function update($id){
+        $akun3 = $this->akun3model->find($id);
+
+        if (!$akun3) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+        $rules = [
+            'id_akun_2' => [
+                'label' => 'Induk Golongan',
+                'rules' => 'required|integer'
+            ],
+            'kode_akun_3' => [
+                'label' => 'Kode Akun 3',
+                'rules' => 'required|numeric|is_unique[akun_3.kode_akun_3,id,' . $id . ']',
+                'errors' => [
+                    'is_unique' => '{field} sudah terdaftar dalam sistem'
+                ]
+            ],
+            'nama_akun_3' => [
+                'label' => 'Nama Akun 3',
+                'rules' => 'required|min_length[3]|max_length[100]'
+            ],
+            'saldo_normal' => [
+                'label' => 'Saldo Normal',
+                'rules' => 'required|in_list[Debit,Kredit]'
+            ]
+        ];
+
+        if (!$this->validate($rules)) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+
+        $this->akun3model->update($id, [
+            'id_akun_2'    => $this->request->getPost('id_akun_2'),
+            'kode_akun_3'  => $this->request->getPost('kode_akun_3'),
+            'nama_akun_3'  => $this->request->getPost('nama_akun_3'),
+            'saldo_normal' => $this->request->getPost('saldo_normal')
+        ]);
+
+        return redirect()->to('/akun3')->with('success', 'Data Akun 3 berhasil diperbarui!');
+    }
+
     public function delete($id){
         $this->akun3model->delete($id);
-        return redirect()->to('/akun3')->with('success', 'Data Akun 3 berhasil');
+        return redirect()->to('/akun3')->with('success', 'Data Akun 3 berhasil dihapus!');
     }
 }
